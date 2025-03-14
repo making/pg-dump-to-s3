95035e268ce8831277e4f864316edc7d5fec0a27
Toshiaki Maki <makingx@gmail.com>
2025-03-14 10:14:18 +0900

    Support additional args

--- src/main/java/lol/maki/batch/PgDumpProps.java
+++ src/main/java/lol/maki/batch/PgDumpProps.java
@@ -1,7 +1,9 @@
 package lol.maki.batch;
 
+import java.util.List;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "pg-dump")
-public record PgDumpProps(String host, int port, String username, String password, String database) {
+public record PgDumpProps(String host, int port, String username, String password, String database,
+		List<String> additionalArgs) {
 }
--- src/main/java/lol/maki/batch/tasklet/DumpTasklet.java
+++ src/main/java/lol/maki/batch/tasklet/DumpTasklet.java
@@ -4,19 +4,20 @@ import java.nio.file.Path;
 import java.time.Clock;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
-
 import lol.maki.batch.PgDumpProps;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.item.ExecutionContext;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
 
 @Component
 public class DumpTasklet implements Tasklet {
@@ -38,9 +39,12 @@ public class DumpTasklet implements Tasklet {
 		final Path tmpDir = Path.of(System.getProperty("java.io.tmpdir"));
 		final String timestamp = LocalDateTime.now(this.clock).format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
 		final Path dump = tmpDir.resolve("%s".formatted(timestamp));
-		final ProcessBuilder processBuilder = new ProcessBuilder("pg_dump", "-U", this.pgDumpProps.username(), "-h",
-				this.pgDumpProps.host(), "-p", String.valueOf(this.pgDumpProps.port()), this.pgDumpProps.database())
-			.directory(tmpDir.toFile())
+		final List<String> args = new ArrayList<>(List.of("pg_dump", "-U", this.pgDumpProps.username(), "-h",
+				this.pgDumpProps.host(), "-p", String.valueOf(this.pgDumpProps.port()), this.pgDumpProps.database()));
+		if (!CollectionUtils.isEmpty(this.pgDumpProps.additionalArgs())) {
+			args.addAll(this.pgDumpProps.additionalArgs());
+		}
+		final ProcessBuilder processBuilder = new ProcessBuilder(args).directory(tmpDir.toFile())
 			.redirectOutput(dump.toFile());
 		final Map<String, String> environment = processBuilder.environment();
 		environment.put("PGPASSWORD", this.pgDumpProps.password());
